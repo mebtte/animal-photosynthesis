0591d8355e094afb5b84da5835dfa784fc007459
mebtte <hi@mebtte.com>
2020-04-28 11:03:31 +0800

    update readme

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Helmet } from 'react-helmet';
+
+const FONT_PATH = '/font/header_font.ttf';
 
 const Style = styled.header`
   @font-face {
     font-family: header_font;
-    src: url('/font/header_font.ttf');
+    src: url(${FONT_PATH});
   }
   margin: 20px;
   font-family: header_font;
@@ -13,6 +16,14 @@ const Style = styled.header`
 const Header = () => {
   return (
     <Style>
+      <Helmet>
+        <link
+          rel="preload"
+          href={FONT_PATH}
+          as="font"
+          crossOrigin="anonymous"
+        />
+      </Helmet>
       <h1>答案</h1>
     </Style>
   );
