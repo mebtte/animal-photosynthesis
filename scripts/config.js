26ca37bb443c816c7601ad9f6c18fe974b282d50
WilliamsZhang <williams_z@yeah.net>
2020-12-27 15:21:00 +0800

    init

--- scripts/config.js
+++ scripts/config.js
@@ -1,10 +1,10 @@
 export default {
   author: 'williamszhang',
-  authorSite: 'http://williamszhang.top/',
+  authorSite: 'http://williamszhang.top',
   keywords: ['大数据', 'Java'],
   title: 'Answer',
   description: "WilliamsZhang's Writing",
-  site: process.env.SITE || '',
-  publicPath: process.env.PUBLIC_PATH || '',
+  // site: process.env.SITE || '',
+  // publicPath: process.env.PUBLIC_PATH || '',
   githubRepository: 'https://github.com/WilliamsZhang/article',
 };
