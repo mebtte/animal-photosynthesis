ee344a1f0d784cab1b9824b5aa84281fd34fe97b
WilliamsZhang <williams_z@yeah.net>
2020-12-27 13:47:28 +0800

    init

--- scripts/config.js
+++ scripts/config.js
@@ -1,10 +1,10 @@
 export default {
-  author: 'mebtte',
-  authorSite: 'https://mebtte.com',
-  keywords: ['HTML', 'CSS', 'JavaScript', 'Web'],
+  author: 'williamszhang',
+  authorSite: 'http://williamszhang.top/',
+  keywords: ['大数据', 'Java'],
   title: 'Answer',
-  description: "MEBTTE's Writing",
-  site: process.env.SITE || 'https://article.mebtte.com',
+  description: "WilliamsZhang's Writing",
+  site: process.env.SITE || '',
   publicPath: process.env.PUBLIC_PATH || '',
   githubRepository: 'https://github.com/WilliamsZhang/article',
 };
