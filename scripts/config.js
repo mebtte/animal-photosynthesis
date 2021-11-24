02ca0b5fac1c75690f25f570a7e142e673e18253
mebtte <hi@mebtte.com>
2021-11-24 12:36:19 +0800

    update style

--- scripts/config.js
+++ scripts/config.js
@@ -2,7 +2,7 @@ export default {
   /** site author */
   author: 'mebtte',
   /** author personal site */
-  author_site: 'https://mebtte.com',
+  author_site: 'https://i.mebtte.com',
   /** site keywords */
   keywords: ['HTML', 'CSS', 'JavaScript', 'Web'],
   /** site title */
@@ -10,7 +10,7 @@ export default {
   /** site description */
   description: "MEBTTE's Writing",
   /** site origin */
-  origin: 'https://article.mebtte.com',
+  origin: 'https://mebtte.com',
   /** site public path, like `/article` or `/blog` */
   public_path: '',
   github_repository: 'https://github.com/mebtte/article',
