12beceadf5d5b571e4839946f77f24bc0a39c7ba
mebtte <hi@mebtte.com>
2023-04-23 16:03:11 +0800

    update description

--- scripts/config.js
+++ scripts/config.js
@@ -3,7 +3,7 @@ export default {
   author_site: 'https://i.mebtte.com',
   keywords: ['HTML', 'CSS', 'JavaScript', 'Web'],
   title: '动物的光合作用',
-  description: 'MEBTTE 的个人写作.',
+  description: 'MEBTTE 的个人博客.',
   origin: 'https://mebtte.com',
   public_path: '',
   github_repository:
