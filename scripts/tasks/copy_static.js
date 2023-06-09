d5dcfd3ac28182a80d40b5dc33e9be7a443df146
mebtte <hi@mebtte.com>
2023-06-09 11:15:21 +0800

    fix build error

--- scripts/tasks/copy_static.js
+++ scripts/tasks/copy_static.js
@@ -1,11 +1,8 @@
-import fs from 'fs-extra';
+import fsPromises from 'fs/promises';
 import DIRECTORY from '../utils/directory.js';
-import * as util from 'util';
-
-const copyAsync = util.promisify(fs.cp);
 
 export default () => {
-  return copyAsync(DIRECTORY.STATIC, DIRECTORY.BUILD, {
+  return fsPromises.cp(DIRECTORY.STATIC, DIRECTORY.BUILD, {
     recursive: true,
     force: true,
   });
