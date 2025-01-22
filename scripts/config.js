5b51fa6c73df53e43cdd32121136c411f4d23b68
mebtte <i@mebtte.com>
2025-01-22 22:41:57 +0800

    update config

--- scripts/config.js
+++ scripts/config.js
@@ -1,8 +1,7 @@
 export default {
-  title: '动物的光合作用',
-  description: 'MEBTTE 的个人博客.',
-  origin: 'https://mebtte.com',
-  public_path: '',
-  github_repository:
-    'https://github.com/mebtte/animal-photosynthesis',
+  title: "动物的光合作用",
+  description: "一头能进行光合作用的动物",
+  origin: "https://mebtte.com",
+  public_path: "",
+  github_repository: "https://github.com/mebtte/animal-photosynthesis",
 };
