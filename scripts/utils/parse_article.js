b8163036f8acd9aafae6f780eb73dc25d23f8b57
mebtte <i@mebtte.com>
2025-02-21 00:01:31 +0800

    using_round-robin_dns_as_load_blancing_under_nat_traversal_on_nas

--- scripts/utils/parse_article.js
+++ scripts/utils/parse_article.js
@@ -1,11 +1,11 @@
-import * as path from 'path';
-import showdown from 'showdown';
-import frontMatter from 'front-matter';
-import cheerio from 'cheerio';
-import fs from './fs.js';
-import directory from './directory.js';
-import toBuild from './to_build.js';
-import config from '../config.js';
+import * as path from "path";
+import showdown from "showdown";
+import frontMatter from "front-matter";
+import cheerio from "cheerio";
+import fs from "./fs.js";
+import directory from "./directory.js";
+import toBuild from "./to_build.js";
+import config from "../config.js";
 
 export default async (id) => {
   const articleDir = `${directory.ARTICLES}/${id}`;
@@ -31,7 +31,7 @@ export default async (id) => {
     for (const resourceNode of resourceNodeList) {
       const node = $(resourceNode);
       const resourcePath = node.attr(type);
-      if (resourcePath[0] !== '.') {
+      if (resourcePath[0] !== ".") {
         continue;
       }
       const localPath = path.join(articleDir, resourcePath);
@@ -39,59 +39,56 @@ export default async (id) => {
       $(resourceNode).attr(type, `/${filename}`);
     }
   };
-  await buildLocalResource('src');
-  await buildLocalResource('href');
+  await buildLocalResource("src");
+  await buildLocalResource("href");
 
   // img
-  const imgNodeList = $('img').toArray();
+  const imgNodeList = $("img").toArray();
   for (const imgNode of imgNodeList) {
     const node = $(imgNode);
-    const src = node.attr('src');
-    const alt = node.attr('alt');
+    const src = node.attr("src");
+    const alt = node.attr("alt");
     node.parent().replaceWith(`
       <figure class="figure-img">
         <a href="${src}" target="_blank">
           <img src="${src}" alt="${alt}" title="${alt}" loading="lazy" />
         </a>
-        ${alt ? `<figcaption>${alt}</figcaption>` : ''}
+        ${alt ? `<figcaption>${alt}</figcaption>` : ""}
       </figure>
     `);
   }
 
-  // video
-  const videoNodeList = $('video').toArray();
-  for (const videoNode of videoNodeList) {
-    const node = $(videoNode);
-    const src = node.attr('src');
-    const alt = node.attr('alt') || '';
+  // audio or video
+  const mediaNodeList = $("video, audio").toArray();
+  for (const mediaNode of mediaNodeList) {
+    const node = $(mediaNode);
+    const src = node.attr("src");
+    const alt = node.attr("alt") || "";
+    const tagName = node.prop("tagName");
     node.replaceWith(`
-      <figure class="figure-video">
-        <video src="${src}" loading="lazy" controls></video>
-        ${alt ? `<figcaption>${alt}</figcaption>` : ''}
+      <figure class="figure-media">
+        <${tagName} class="media" src="${src}" loading="lazy" controls></${tagName}>
+        ${alt ? `<figcaption>${alt}</figcaption>` : ""}
       </figure>
     `);
   }
 
   // iframe
-  const iframeNodeList = $('iframe').toArray();
+  const iframeNodeList = $("iframe").toArray();
   for (const iframeNode of iframeNodeList) {
     const node = $(iframeNode);
-    const nodeTitle = node.attr('title');
-    const src = node.attr('src');
+    const nodeTitle = node.attr("title");
+    const src = node.attr("src");
     node.replaceWith(`
       <figure class="figure-iframe">
         <iframe src="${src}" title="${nodeTitle}" loading="lazy"></iframe>
-        ${
-          nodeTitle
-            ? `<figcaption>${nodeTitle}</figcaption>`
-            : ''
-        }
+        ${nodeTitle ? `<figcaption>${nodeTitle}</figcaption>` : ""}
       </figure>
     `);
   }
 
   // table
-  const tableNodeList = $('table').toArray();
+  const tableNodeList = $("table").toArray();
   for (const tableNode of tableNodeList) {
     const node = $(tableNode);
     const nodeHtml = node.html();
@@ -105,7 +102,7 @@ export default async (id) => {
   }
 
   // code
-  const codeNodeList = $('code').toArray();
+  const codeNodeList = $("code").toArray();
   for (const codeNode of codeNodeList) {
     const node = $(codeNode);
     node.html(node.html());
@@ -113,14 +110,12 @@ export default async (id) => {
 
   const article = {
     id,
-    title: attributes.title || '',
+    title: attributes.title || "",
     // 取 markdown 内容的前 150 个字符作为页面的 description
-    description:
-      mdBody.substring(0, 150).replace(/\s/gm, ' ') + '...',
+    description: mdBody.substring(0, 150).replace(/\s/gm, " ") + "...",
     publishTime: attributes.publish_time,
     updates: attributes.updates || [],
-    hidden:
-      !attributes.publish_time || attributes.hidden || false,
+    hidden: !attributes.publish_time || attributes.hidden || false,
     content: $.html(),
     mdText,
   };
